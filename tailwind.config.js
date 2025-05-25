cf8d575dcaf8fc2ff61f777313962becf4c35035
DiogoLaptop <diogonascimentonovo@gmail.com>
2025-05-24 22:34:21 -0300

    primeiro deploy front

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,html}"],
-  theme: { extend: {} },
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './precisoja-front/**/*.{html,js}', // ⬅️ diz onde o Tailwind deve escanear por classes
+  ],
+  theme: {
+    extend: {},
+  },
   plugins: [],
 }
+  
--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,17 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 
-export default defineConfig({})
+export default defineConfig({
+  root: 'precisoja-front',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'precisoja-front'),
+      '@js': path.resolve(__dirname, 'precisoja-front/js'),
+      '@css': path.resolve(__dirname, 'precisoja-front/style.css')
+    }
+  },
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true
+  }
+})
